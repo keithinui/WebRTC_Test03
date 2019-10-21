0d7c64a406bde50857545f059d3af411817777ee
keithinui <55673645+keithinui@users.noreply.github.com>
2019-10-21 10:16:16 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var textPR = document.getElementById('textPR');
 var localSendText = document.getElementById('js-local-text');
+var batteryLevel;
 
 textPR.addEventListener('click', function() {
   textPR.textContent = '..';
@@ -7,7 +8,8 @@ textPR.addEventListener('click', function() {
   heartRateSensor.connect()
   .then(() => heartRateSensor.startNotificationsHeartRateMeasurement().then(handleHeartRateMeasurement))
   .catch(error => {
-    textPR.textContent = error;
+    const messages = document.getElementById('js-messages');
+    messages.textContent = error;
   });
 });
 
@@ -15,6 +17,9 @@ function handleHeartRateMeasurement(heartRateMeasurement) {
   heartRateMeasurement.addEventListener('characteristicvaluechanged', event => {
     var heartRateMeasurement = heartRateSensor.parseHeartRate(event.target.value);
     textPR.innerHTML = heartRateMeasurement.heartRate;
+
+    heartRateSensor.getBatteryLevel();
+    statusBatteryLavel.textContent = batteryLevel;
 
     if(peer.open){
         room.send(textPR.innerHTML);
